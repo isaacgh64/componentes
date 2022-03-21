63955ff165b5262245c27506a4f0387d8f85a24b
isaac <isaacgallardo@iesflorenciopintado.es>
2022-03-21 16:50:16 +0100

    leemos Json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,31 +1,43 @@
+import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Componentes'),
+        title: const Text('Componentes'),
       ),
       body: _lista(),
     );
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data ?? []),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return const [
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    // ignore: avoid_function_literals_in_foreach_calls
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: const Icon(Icons.access_alarm, color: Colors.blue),
+        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(const Divider());
+    });
+    return opciones;
   }
 }
