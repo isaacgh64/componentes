dedc933c03330c247b4bab2cb9d8695277a33e62
isaac <isaacgallardo@iesflorenciopintado.es>
2022-03-21 11:26:20 +0100

    App_Componentes

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    return ListView(
+      children: _listaItems(),
+    );
+  }
+
+  List<Widget> _listaItems() {
+    return const [
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+      ListTile(title: Text('Hola Mundo')),
+    ];
+  }
+}
